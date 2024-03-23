ea6635d347b0dcfe8a81683761f1de811882e7bf
Kadachii <kadachii@protonmail.com>
2024-03-23 01:13:40 -0700

    Add project files.

---
+++ GiggleSharp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using VRC.OSCQuery;
+using Microsoft.Extensions.Configuration.Ini;
+
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+namespace Giggletech.OSCRouter
+{
+    class Router
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
